aaae39dd2e8fdc95ef9f123d0e5d144bc240066f
AliAbdollahian <ALIABDOLLAHIAN@cmail.carleton.ca>
2025-01-02 17:31:02 -0500

    Sanity check and basic integration health check added

---
+++ src/test/java/com/capstone/lightalert/HttpRequestAlertTest.java
@@ -0,0 +1,21 @@
+package com.capstone.lightalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestAlertTest {
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Test
+    void alertControllerShouldReturnAlert(){
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/alert", String.class)).contains("alert");
+    }
+}
--- src/test/java/com/capstone/lightalert/LightAlertApplicationTests.java
+++ src/test/java/com/capstone/lightalert/LightAlertApplicationTests.java
@@ -1,13 +1,18 @@
 package com.capstone.lightalert;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class LightAlertApplicationTests {
 
+    @Autowired
+    private HealthController healthController;
     @Test
     void contextLoads() {
+        assertThat(healthController).isNotNull();
     }
-
 }
